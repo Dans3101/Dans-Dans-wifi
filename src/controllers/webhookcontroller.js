80287b369289229695d75d7f6551cc95e0a598a0
Dans3101 <sanwhat898@gmail.com>
2025-11-29 12:00:24 +0300

    Update webhookcontroller.js

--- src/controllers/webhookcontroller.js
+++ src/controllers/webhookcontroller.js
@@ -1,1 +1,14 @@
-1
+import db from "../utils/db.js";
+
+export const mpesaCallback = async (req, res) => {
+  console.log("M-Pesa Callback Received:", req.body);
+
+  const checkoutID = req.body.Body.stkCallback.CheckoutRequestID;
+
+  await db.run(
+    "UPDATE sessions SET status = ? WHERE checkoutID = ?",
+    ["paid", checkoutID]
+  );
+
+  res.status(200).send("OK");
+};
