db38cb3344bb8ae7b1b590d9335b6990c59a09c4
Dans3101 <sanwhat898@gmail.com>
2025-12-09 10:51:50 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,15 @@
-1
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./db/database.sqlite");
+
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS payments(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        phone TEXT,
+        amount TEXT,
+        mpesa_receipt TEXT,
+        status TEXT,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+    )`);
+});
+
+module.exports = db;
