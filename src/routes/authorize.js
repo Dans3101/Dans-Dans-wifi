3ca97c138c495499371d532d4017fc057632507f
Dans3101 <sanwhat898@gmail.com>
2025-12-12 14:16:38 +0300

    Delete src/routes directory

--- src/routes/authorize.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { authorizeDevice } from "../controllers/authorizeController.js";
-
-const router = express.Router();
-router.get("/", authorizeDevice);
-
-export default router;
--- src/routes/payment.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { startPayment } from "../controllers/paymentController.js";
-
-const router = express.Router();
-router.post("/", startPayment);
-
-export default router;
